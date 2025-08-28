b08faa99b27b2a7d173f8ab40e689556457cb680
David_one8 <davidfule18@gmail.com>
2025-08-28 21:35:33 +0530

    chore: remove Firebase configuration file

--- config/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const Firebase = require('firebase-admin');
-const serviceAccount = require('../ultimatestore-2eb0e-firebase-adminsdk-fbsvc-5342babe93.json');
-
-Firebase.initializeApp({
-  credential: Firebase.credential.cert(serviceAccount),
-  storageBucket: 'ultimatestore-2eb0e.appspot.com',
-});
-
-module.exports = Firebase;
